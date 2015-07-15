793c46aee8c46e5989655a3a633c8785362de8e9
Benjamin Chavez <bhcg20041989@gmail.com>
2015-07-15 13:46:14 -0500

    Nueva funciona
    Agregando la función de reordenado de arrays

--- JS/Functions.js
+++ JS/Functions.js
@@ -40,3 +40,8 @@ function password(length,special)
   	}
   	return password;
 }
+// Funcion para des-Ordenar un arreglo
+function Shuffle(o) {
+  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
+};
